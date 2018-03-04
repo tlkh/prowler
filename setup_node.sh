c5ec449eaaf8bc6b2968013616735707d0e9c535
Timothy Liu <tlkh.xms@gmail.com>
2018-03-04 17:31:21 +0000

    fixed requirements file

--- setup_node.sh
+++ setup_node.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Updating device..."
 sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
-sudo apt autoremove
+sudo apt autoremove && sudo apt clean
 
+echo "Configuring environment..."
 sudo apt install python3-dev python3-pip python3-numpy
 sudo apt install nmap
 sudo pip3 install setuptools wheel pip --upgrade
